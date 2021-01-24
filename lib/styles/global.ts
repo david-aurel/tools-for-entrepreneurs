1012f4864308afe1c456e826c3447e0616ac66d5
David Durlan <david.a.durlan@gmail.com>
2021-01-24 13:07:23 +0100

    💅 update global styles

--- lib/styles/global.ts
+++ lib/styles/global.ts
@@ -2,9 +2,11 @@ import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
   body {
+    padding: 30px;
     font-family: "Roboto", sans-serif;
     background-color: ${({ theme }) => theme.color.background};
-    color: ${({ theme }) => theme.color.background};
+    color: ${({ theme }) => theme.color.text};
+    word-break: break-word;
   }
 `
 
--- lib/styles/theme.ts
+++ lib/styles/theme.ts
@@ -1,7 +1,11 @@
 const light = {
   color: {
-    primary: 'rebeccapurple',
-    background: 'white',
+    primary: '#FFC6C7',
+    background: '#FAEEE7',
+    text: '#34272A',
+    contrast: 'white',
+    mediumContrast: '#34272A',
+    card: 'white',
   },
 }
 
@@ -9,13 +13,18 @@ export type Color = typeof light
 
 const dark: Color = {
   color: {
-    primary: '#d793ec',
-    background: '#333',
+    primary: '#FFC6C7',
+    background: '#34272A',
+    text: '#FAEEE7',
+    contrast: '#211212',
+    mediumContrast: '#211212',
+    card: '#211212',
   },
 }
 
 const defaultTheme = {
   mq: '(min-width: 500px)',
+  borderRadius: '5px',
 }
 
 export const lightTheme = { ...defaultTheme, ...light }
